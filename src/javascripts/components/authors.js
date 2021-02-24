4d5d56621271da1c53aeafa643a345d88dc8ce83
Chie Stroud <urirunrunkoko@gmail.com>
2021-02-23 22:14:57 -0600

    added function to display books and list of authors

--- src/javascripts/components/authors.js
+++ src/javascripts/components/authors.js
@@ -1,12 +1,19 @@
 // FIXME: STUDENTS show your authors
-
 const showAuthors = (array) => {
   document.querySelector('#store').innerHTML = '';
   // CREATE A BUTTON TO ADD BOOKS
 
   array.forEach((item) => {
     // FIXME: STUDENTS create cards for your authors
-    document.querySelector('#store').innerHTML += `${item}`;
+    document.querySelector('#store').innerHTML += `<div class="card">
+                                                    <div class="card-body" style="height: 180px;">
+                                                    <h5 class="card-title">${item.first_name} ${item.last_name}</h5>
+                                                    <h5 class="card-title">${item.email}</h5>
+                                                    <hr>
+                                                    <button class="btn btn-info" data-toggle="modal" data-target="#formModal" id="edit-book-btn--${item.firebaseKey}">Edit Author</button>
+                                                    <button class="btn btn-danger" id="delete-book--${item.firebaseKey}">Delete Author</button>
+                                                    </div>
+                                                  </div>`;
   });
 };
 
--- src/javascripts/events/navigationEvents.js
+++ src/javascripts/events/navigationEvents.js
@@ -1,4 +1,6 @@
+import { showAuthors, emptyAuthors } from '../components/authors';
 import signOut from '../helpers/auth/signOut';
+import getAuthors from '../helpers/data/authorData';
 
 // navigation events
 const navigationEvents = () => {
@@ -35,6 +37,9 @@ const navigationEvents = () => {
   // 1. When a user clicks the authors link, make a call to firebase to get all authors
   // 2. Convert the response to an array because that is what the makeAuthors function is expecting
   // 3. If the array is empty because there are no authors, make sure to use the emptyAuthor function
+  document.querySelector('#authors').addEventListener('click', () => {
+    getAuthors().then((authors) => showAuthors(authors)).catch(() => emptyAuthors());
+  });
 };
 
 export default navigationEvents;
